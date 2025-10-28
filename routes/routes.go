5bb009d1d849e9f7448e4e73cc254ef468aff2d5
Oluwatise Ajayi <dr393462@gmail.com>
2025-10-28 23:31:21 +0100

    initial commit

---
+++ routes/routes.go
@@ -0,0 +1,31 @@
+package routes
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/shemigam1/hngxiii-currency-exchange/services"
+)
+
+// POST /countries/refresh → Fetch all countries and exchange rates, then cache them in the database
+// GET /countries → Get all countries from the DB (support filters and sorting) - ?region=Africa | ?currency=NGN | ?sort=gdp_desc
+// GET /countries/:name → Get one country by name
+// DELETE /countries/:name → Delete a country record
+// GET /status → Show total countries and last refresh timestamp
+// GET /countries/image → serve summary image
+func Routes(r *gin.Engine) {
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Welcome to HNGXIII Currency Exchange API",
+		})
+	})
+
+	countriesGroup := r.Group("/countries")
+	{
+		countriesGroup.POST("/refresh", services.RefreshCountries)
+		countriesGroup.GET("/", services.GetAllCountries)
+		countriesGroup.DELETE("/:name", services.DeleteCountry)
+		countriesGroup.GET("/:name", services.GetCountry)
+	}
+
+	r.GET("/status", services.GetStatus)
+	r.GET("/countries/image", services.GetSummaryImage)
+}
