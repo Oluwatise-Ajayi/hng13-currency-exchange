a5da368fd655b30f0235839b753079147eddf243
Oluwatise Ajayi <dr393462@gmail.com>
2025-10-29 00:44:41 +0100

    Enhance migration process with logging and error handling for database migrations

--- migrations/migrate.go
+++ migrations/migrate.go
@@ -1,15 +1,28 @@
 package main
 
 import (
+	"log" // <-- Make sure to import "log"
+
 	"github.com/Oluwatise-Ajayi/hng13-currency-exchange/db"
 	"github.com/Oluwatise-Ajayi/hng13-currency-exchange/models"
 )
 
 func init() {
 	db.LoadEnvVariables()
-	db.ConnectDB()
+	db.ConnectDB() // We'll assume this logs a fatal error if it fails
 }
 
 func main() {
-	db.DB.AutoMigrate(&models.CountryInfo{})
+	log.Println("Attempting to run database migrations...")
+
+	// Capture the error from AutoMigrate
+	err := db.DB.AutoMigrate(&models.CountryInfo{})
+	
+	if err != nil {
+		// If there's ANY error, print it and CRASH the program.
+		log.Fatalf("FATAL: Failed to run migrations: %v\n", err)
+	}
+
+	// This line will ONLY be printed if the migration was successful
+	log.Println("Database migrated successfully.")
 }
